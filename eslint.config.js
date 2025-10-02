fa2bdf2fc7aff744294ad3c6f145721ecab7e0c3
Ken-Japa <kenjapa@gmail.com>
2025-10-01 23:22:10 -0300

    feat: Configurações iniciais do ESLint e Prettier, e atualização de dependências.

--- eslint.config.js
+++ eslint.config.js
@@ -1,74 +1,17 @@
-import globals from 'globals'
-import unusedImports from 'eslint-plugin-unused-imports'
-import tseslint from 'typescript-eslint'
-import nextPlugin from '@next/eslint-plugin-next'
-import simpleImportSort from 'eslint-plugin-simple-import-sort'
-import { FlatCompat } from '@eslint/eslintrc'
-import prettierPlugin from 'eslint-plugin-prettier'
+import prettierPlugin from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
 
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-})
-
-const config = [
+export default [
   {
-    files: ['**/*.{js,cjs,ts,jsx,tsx}'], // Removido .mjs daqui
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-          // Adicionando 'jsx-runtime' para o novo JSX transform
-          'jsx-runtime': true,
-        },
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        project: ['./tsconfig.json'],
-      },
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        ...globals.jest,
-      },
-    },
     plugins: {
-      'simple-import-sort': simpleImportSort,
-      '@next/next': nextPlugin,
-      '@typescript-eslint': tseslint.plugin,
-      'unused-imports': unusedImports,
       prettier: prettierPlugin,
+      "react-hooks": reactHooksPlugin,
     },
+    extends: [eslintConfigPrettier],
     rules: {
-      'simple-import-sort/imports': 'off',
-      'simple-import-sort/exports': 'off',
-      'react/display-name': 'off',
-      'no-case-declarations': 'off',
-      'no-constant-binary-expression': 'off',
-      'no-empty': 'off',
-      '@typescript-eslint/no-empty-function': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-      'unused-imports/no-unused-imports': 'off',
-      'unused-imports/no-unused-vars': 'off',
-      ...nextPlugin.configs.recommended.rules,
-      ...nextPlugin.configs['core-web-vitals'].rules,
-      'prettier/prettier': 'error',
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
+      "prettier/prettier": "error",
+      "react-hooks/rules-of-hooks": "error",
     },
   },
-  // Adiciona eslint-config-prettier como a última configuração para estender
-  {
-    extends: ['prettier'],
-  },
-  ...compat
-    .config({
-      extends: ['next/core-web-vitals'],
-    })
-    .map((config) => ({ ...config, files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] })),
-]
-
-export default config
+];
